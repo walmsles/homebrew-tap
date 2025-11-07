9e0d3a8044a6829d948f07b2bd739c9b7d5b0923
Michael Walmsley <m.walmsley@accenture.com>
2025-11-07 15:07:56 +1100

    Update azure2aws to v1.0.1 with correct checksum

--- Formula/azure2aws.rb
+++ Formula/azure2aws.rb
@@ -2,7 +2,7 @@ class Azure2aws < Formula
   desc "Azure to AWS credential conversion utility"
   homepage "https://github.com/walmsles/azure2aws"
   url "https://github.com/walmsles/azure2aws/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "bb7384080c73105d3f4d5827ba3ba9e03cc7594f47f9744aa16ac5e6fe480ba4"
   version "1.0.1"
   license "MIT"
 
