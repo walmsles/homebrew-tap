c75cea00cc95e6f9cac37273586ccdf869e6806d
Michael Walmsley <m.walmsley@accenture.com>
2025-11-07 15:26:03 +1100

    Update azure2aws to v1.0.2

--- Formula/azure2aws.rb
+++ Formula/azure2aws.rb
@@ -1,9 +1,9 @@
 class Azure2aws < Formula
   desc "Azure to AWS credential conversion utility"
   homepage "https://github.com/walmsles/azure2aws"
-  url "https://github.com/walmsles/azure2aws/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "bb7384080c73105d3f4d5827ba3ba9e03cc7594f47f9744aa16ac5e6fe480ba4"
-  version "1.0.1"
+  url "https://github.com/walmsles/azure2aws/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "89c44114417f66c510576faf3ddec5fb8b02986e9c28619082d5f89235b5dbdd"
+  version "1.0.2"
   license "MIT"
 
   depends_on "go" => :build
