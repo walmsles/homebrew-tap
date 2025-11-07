81c41d1436f445eb28f8c3da3217230de8c8ca8f
Michael Walmsley <m.walmsley@accenture.com>
2025-11-07 14:49:14 +1100

    Use make build in formula for proper version stamping

--- Formula/azure2aws.rb
+++ Formula/azure2aws.rb
@@ -1,18 +1,19 @@
 class Azure2aws < Formula
   desc "Azure to AWS credential conversion utility"
   homepage "https://github.com/walmsles/azure2aws"
-  url "https://github.com/walmsles/azure2aws/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "7005a2b1b014a9d45e84dfccc8344b1001e0fc8c547b6545549ef8d30632ec7b"
-  version "1.0.0"
+  url "https://github.com/walmsles/azure2aws/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  version "1.0.1"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    system "make", "build"
+    bin.install "azure2aws"
   end
 
   test do
-    system "#{bin}/azure2aws", "--help"
+    system "#{bin}/azure2aws", "--version"
   end
 end
